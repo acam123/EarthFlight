d1dfef24c86392b302400e051a163556f5eefa1b
AidanCampbell <aidancalebcampbell@gmail.com>
2014-01-21 11:53:17 -0500

    initialized app with git

---
+++ js/passengers.js
@@ -0,0 +1,91 @@
+/**
+ * passengers.js
+ *
+ * Computer Science 50
+ * Earth Flight
+ *
+ * Passengers on campus.
+ */
+  
+     HOOPS = [
+    {
+        number: "0",
+        lat: 42.3745615030193,
+        lng: -71.11803936751632,
+        alt: 20,
+        tilt: 0,
+        rotate: -90
+    },
+    {
+        number: "1",
+        lat: 42.3747615030193,
+        lng: -71.11853936751632,
+        alt: 20,
+        tilt: 0,
+        rotate: -90
+    },
+    {
+        number: "2",
+        lat: 42.3749615030193,
+        lng: -71.11893936751632,
+        alt: 20,
+        tilt: 0,
+        rotate: -180
+    },
+    {
+        number: "3",
+        lat: 42.3746597350193,
+        lng: -71.11845936254676,
+        alt: 20,
+        tilt: 20,
+        rotate: 90
+    },
+    {
+        number: "4",
+        lat: 42.3740475832793,
+        lng: -71.119939363541632,
+        alt: 20,
+        tilt: 15,
+        rotate: -90
+    },
+    {
+        number: "5",
+        lat: 42.374061589763493,
+        lng: -71.118939367451572,
+        alt: 20,
+        tilt: 0,
+        rotate: -45
+    },
+    {
+        number: "6",
+        lat: 42.3747802069373,
+        lng: -71.11523946752372,
+        alt: 20,
+        tilt: 20,
+        rotate: 15
+    },
+    {
+        number: "7",
+        lat: 42.374605872193,
+        lng: -71.110377820651632,
+        alt: 20,
+        tilt: 10,
+        rotate: -60
+    },
+    {
+        number: "8",
+        lat: 42.3750007860373,
+        lng: -71.11696840-375937,
+        alt: 20,
+        tilt: 0,
+        rotate: 75
+    },
+    {
+        number: "9",
+        lat: 42.37465693950193,
+        lng: -71.11234691246632,
+        alt: 20,
+        tilt: 0,
+        rotate: -90
+    }
+];
---
+++ js/service.js
@@ -0,0 +1,536 @@
+/**
+ * service.js
+ *
+ * Computer Science 50
+ * Earth Flight
+ *
+ * Implements a shuttle service.
+ */
+
+// default height
+var HEIGHT = 0.8;
+
+// default latitude
+var LATITUDE = 42.3745615030193;
+
+// default longitude
+var LONGITUDE = -71.11803936751632;
+
+// default heading
+var HEADING = 1.757197490907891;
+
+// default velocity
+var VELOCITY = 100;
+
+// global reference to shuttle's marker on 2D map
+var planeicon = null;
+
+// global reference to 3D Earth
+var earth = null;
+
+// global reference to 2D map
+var map = null;
+
+// global reference to shuttle
+var shuttle = null;
+
+// initialize force
+var FORCE = 0;
+
+// initialize plane view
+var plane_view = 0;
+
+//initialize points
+var score = 0;
+
+// load version 1 of the Google Earth API
+google.load("earth", "1");
+
+// load version 3 of the Google Maps API
+google.load("maps", "3", {other_params: "sensor=false"});
+
+// once the window has loaded
+$(window).load(function() {
+
+    // listen for keydown anywhere in body
+    $(document.body).keydown(function(event) {
+        change_plane_mode(event);     
+        return keystroke(event, true);
+    });
+
+    // listen for keyup anywhere in body
+    $(document.body).keyup(function(event) {
+        return keystroke(event, false);
+    });
+
+    // load application
+    load();
+});
+
+// unload application
+$(window).unload(function() {
+    unload();
+});
+
+
+
+/**
+ * Called if Google Earth fails to load.
+ */
+function failureCB(errorCode) 
+{
+    // report error unless plugin simply isn't installed
+    if (errorCode != ERR_CREATE_PLUGIN)
+    {
+        alert(errorCode);
+    }
+}
+
+/**
+ * Handler for Earth's frameend event.
+ */
+function frameend() 
+{
+    angle_standard();
+    shuttle.update();
+    pickup();
+    move_plane();
+}
+
+/**
+ * Called once Google Earth has loaded.
+ */
+function initCB(instance) 
+{
+    // retain reference to GEPlugin instance
+    earth = instance;
+
+    // specify the speed at which the camera moves
+    earth.getOptions().setFlyToSpeed(100);
+
+    // show buildings
+    earth.getLayerRoot().enableLayerById(earth.LAYER_BUILDINGS, true);
+
+    // disable terrain (so that Earth is flat)
+    earth.getLayerRoot().enableLayerById(earth.LAYER_TERRAIN, false);
+
+    // prevent mouse navigation in the plugin
+    earth.getOptions().setMouseNavigationEnabled(false);
+
+    // instantiate shuttle
+    shuttle = new Shuttle({
+        heading: HEADING,
+        height: HEIGHT,
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        planet: earth,
+        velocity: VELOCITY
+    });
+
+    // synchronize camera with Earth
+    google.earth.addEventListener(earth, "frameend", frameend);
+
+    // synchronize map with Earth
+     google.earth.addEventListener(earth.getView(), "viewchange", viewchange);
+
+    // update shuttle's camera
+    shuttle.updateCamera();
+
+    // show Earth
+    earth.getWindow().setVisibility(true);
+
+
+    // populate Earth with plane
+    populate();
+}
+
+/**
+ * Handles keystrokes.
+ */
+function keystroke(event, state)
+{
+    // ensure we have event
+    if (!event)
+    {
+        event = window.event;
+    }
+
+    // left arrow
+    if (event.keyCode == 37)
+    {
+        shuttle.states.turningLeftward = state;   
+        return false;
+    }
+
+    // up arrow
+    else if (event.keyCode == 38)
+    {
+        shuttle.states.tiltingUpward = state;
+        return false;
+    }
+
+    // right arrow
+    else if (event.keyCode == 39)
+    {
+        shuttle.states.turningRightward = state;
+        return false;
+    }
+
+    // down arrow
+    else if (event.keyCode == 40)
+    {
+        shuttle.states.tiltingDownward = state;
+        return false;
+    }
+
+    // A, a
+    else if (event.keyCode == 65 || event.keyCode == 97)
+    {
+        shuttle.states.slidingLeftward = state;
+        return false;
+    }
+
+    
+    // D, d
+    else if (event.keyCode == 68 || event.keyCode == 100)
+    {
+        shuttle.states.slidingRightward = state;
+        return false;
+    }
+  
+    // S, s
+    else if (event.keyCode == 83 || event.keyCode == 115)
+    {
+        shuttle.states.movingBackward = state; 
+        var direction = "backward";
+        return false;
+    }
+
+    // W, w
+    else if (event.keyCode == 87 || event.keyCode == 119)
+    {
+       // move_plane();
+        shuttle.states.movingForward = state;
+        var direction = "forward";
+    //    force(direction); 
+        return false;
+    }
+    
+    // space
+    else if (event.keyCode == 32)
+    {
+        shuttle.states.flyingUpward = state;
+        return false;
+    }
+    
+    // shift
+    else if (event.keyCode == 16 )
+    {
+        shuttle.states.flyingDownward = state;
+        return false;
+    }
+    
+    return true;
+}
+
+/**
+ * Loads application.
+ */
+function load()
+{
+    // embed 2D map in DOM
+    var latlng = new google.maps.LatLng(LATITUDE, LONGITUDE);
+    map = new google.maps.Map($("#map").get(0), {
+        center: latlng,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        scrollwheel: false,
+        zoom: 16,
+        zoomControl: true,
+        rotation: 90
+    });
+    
+    // prepare plane's icon for map
+     var url = window.location.href.substring(0, (window.location.href.lastIndexOf("/")) + 1);
+     planeicon = new google.maps.Marker({
+       // icon: "https://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/plane.png",
+        icon: (url + "/img/rotations/aircraftsmall0.png"),
+        map: map,
+        title: "you are here",
+    }); 
+
+    // embed 3D Earth in DOM
+    google.earth.createInstance("earth", initCB, failureCB); 
+}
+
+
+/**
+ * Populates Earth with objects
+ */
+function populate()
+{   
+    // get current URL, sans any filename
+    var url = window.location.href.substring(0, (window.location.href.lastIndexOf("/")) + 1);
+    make_overlay(url);
+    make_hoops(url); 
+}
+
+/**
+ * Handler for Earth's viewchange event.
+ */
+function viewchange() 
+{
+    // keep map centered on shuttle's marker
+    var latlng = new google.maps.LatLng(shuttle.position.latitude, shuttle.position.longitude);
+    map.setCenter(latlng);
+
+    var url = window.location.href.substring(0, (window.location.href.lastIndexOf("/")) + 1);
+    
+    if (((shuttle.headingAngle * 180 / Math.PI) > 0) && ((shuttle.headingAngle * 180 / Math.PI )<= 90))
+    {
+        var deg = 0
+    }
+    else if (((shuttle.headingAngle * 180 / Math.PI) > 90) && ((shuttle.headingAngle * 180 / Math.PI )<= 180))
+    {
+        var deg = 3
+    }
+    else if (((shuttle.headingAngle * 180 / Math.PI) > 180) && ((shuttle.headingAngle * 180 / Math.PI )<= 270))
+    {
+        var deg = 2
+    }
+    else if (((shuttle.headingAngle * 180 / Math.PI) > 270) && ((shuttle.headingAngle * 180 / Math.PI )<= 360))
+    {
+        var deg = 1
+    }
+    planeicon.setIcon(url + "/img/rotations/aircraftsmall" + deg +".png");
+    planeicon.setPosition(latlng);
+   
+} 
+
+/**
+ * Unloads Earth.
+ */
+function unload()
+{
+    google.earth.removeEventListener(earth.getView(), "viewchange", viewchange);
+    google.earth.removeEventListener(earth, "frameend", frameend);
+}
+
+function move_plane()
+{  
+     var loc = earth.createLocation('');
+     loc.setLatLngAlt(shuttle.position.latitude -.0000025, shuttle.position.longitude + .0000025, shuttle.cameraAltitude);
+     model.setLocation(loc);
+     var orientation = earth.createOrientation('');
+     orientation.setHeading(shuttle.headingAngle * 180 / Math.PI -100);
+     orientation.setTilt(0);
+     orientation.setRoll(0);
+     model.setOrientation(orientation);
+}
+
+function force(direction)
+{
+    if (direction == "forward")
+    {
+        FORCE += 1; 
+    }
+    else if (direction == "backward")
+    {
+        FORCE -= 1; 
+    }
+    
+    var lookAt = earth.getView().copyAsLookAt(earth.ALTITUDE_RELATIVE_TO_GROUND);
+    lookAt.setLatitude(lookAt.getLatitude() + FORCE);
+    lookAt.setLongitude(lookAt.getLongitude() + 0);
+    earth.getView().setAbstractView(lookAt);
+}
+
+function make_overlay(url)
+{
+     // Create the ScreenOverlay
+     screenOverlay = earth.createScreenOverlay('');
+
+    // Specify a path to the image and set as the icon
+    var icon = earth.createIcon('');
+    //icon.setHref('http://earth-api-samples.googlecode.com/svn/trunk/examples/static/frame.png'); 
+    //icon.setHref(url + "/img/frame.png");
+    //icon.setHref('http://comps.fotosearch.com/comp/CSP/CSP990/metal-window-frame_~k11514638.jpg');
+    //icon.setHref(url + "/img/plane2.png");
+    icon.setHref(url + "/img/cockpit.png");
+    screenOverlay.setIcon(icon);
+
+    // Set the ScreenOverlay's position in the window
+    screenOverlay.getOverlayXY().setXUnits(earth.UNITS_FRACTION);
+    screenOverlay.getOverlayXY().setYUnits(earth.UNITS_FRACTION);
+    //screenOverlay.getOverlayXY().setX(600);
+    //screenOverlay.getOverlayXY().setY(300);
+    screenOverlay.getOverlayXY().setX(.5);
+    screenOverlay.getOverlayXY().setY(.5);
+
+    // Set the overlay's size in pixels
+    screenOverlay.getSize().setXUnits(earth.UNITS_FRACTION);
+    screenOverlay.getSize().setYUnits(earth.UNITS_FRACTION);
+    //screenOverlay.getSize().setX(250);
+    //screenOverlay.getSize().setY(75);
+    screenOverlay.getSize().setX(1);
+    screenOverlay.getSize().setY(1);
+
+    // Specify the point in the image around which to rotate
+    screenOverlay.getRotationXY().setXUnits(earth.UNITS_FRACTION);
+    screenOverlay.getRotationXY().setYUnits(earth.UNITS_FRACTION);
+    screenOverlay.getRotationXY().setX(0.5);
+    screenOverlay.getRotationXY().setY(0.5);
+
+    // Rotate the overlay
+    screenOverlay.setRotation(0);
+
+    // Add the ScreenOverlay to Earth
+    earth.getFeatures().appendChild(screenOverlay);
+}
+
+function make_hoops(url)
+{
+    for (var i = 0; i < HOOPS.length; i++)
+    {
+        var placemark = earth.createPlacemark('');
+        placemark.setName(HOOPS[i].number);
+        var model = earth.createModel('');
+        var link = earth.createLink('');
+        link.setHref(url + "/img/hoop.dae");
+        model.setLink(link); 
+        var loc = earth.createLocation('');
+        loc.setLatLngAlt((HOOPS[i]["lat"]), (HOOPS[i]["lng"]), (HOOPS[i]["alt"]) );
+        var orientation = earth.createOrientation('');
+        orientation.setHeading(HOOPS[i]["rotate"]);
+        orientation.setTilt(HOOPS[i]["tilt"]);
+        model.setOrientation(orientation);
+        model.setLocation(loc);
+        model.setAltitudeMode(earth.ALTITUDE_RELATIVE_TO_GROUND);
+        
+        placemark.setGeometry(model); 
+        earth.getFeatures().appendChild(placemark);
+        
+        // add marker to map
+        var marker = new google.maps.Marker({
+            icon: (url + "/img/star.png"),
+            map: map,
+            position: new google.maps.LatLng(HOOPS[i]["lat"], HOOPS[i]["lng"]),
+            title: "hoop" + i
+            });
+        
+        // remember hoops's placemark and marker for later
+        HOOPS[i]["marker"] = marker;
+        HOOPS[i]["placemark"] = placemark;
+        
+        //set hoops's status
+    }   HOOPS[i]["status"] = "incomplete";
+}
+
+function make_plane(url)
+{
+     
+    // Create a 3D model, initialize it from a Collada file, and place it in the world.
+    placemark = earth.createPlacemark('');
+    placemark.setName('Plane');
+    model = earth.createModel('');
+    link = earth.createLink('');
+    //link.setHref('http://earth-api-samples.googlecode.com/svn/trunk/examples/static/splotchy_box.dae');
+    //link.setHref("/home/jharvard/vhosts/localhost/public/Final_Project2/img/plane.dae");
+    link.setHref(url + "/img/plane2.dae");
+    model.setLink(link);
+    var loc = earth.createLocation('');
+    loc.setLatLngAlt(shuttle.position.latitude -.0000025, shuttle.position.longitude + .0000025, shuttle.cameraAltitude );
+    var orientation = earth.createOrientation('');
+    orientation.setHeading(shuttle.headingAngle * 180 / Math.PI -100);
+    orientation.setTilt(0);
+    model.setOrientation(orientation);
+    model.setLocation(loc);
+    placemark.setGeometry(model); 
+    model.setAltitudeMode(earth.ALTITUDE_RELATIVE_TO_GROUND);
+    earth.getFeatures().appendChild(placemark); 
+}
+
+function pickup()
+{
+      if (shuttle.cameraAltitude >= HOOPS[0]["alt"] - 2 && shuttle.cameraAltitude <= HOOPS[0]["alt"] + 2)
+    {
+        // for each HOOP
+        for (var i = 0; i < HOOPS.length; i++)
+        {
+            if (HOOPS[i]["status"] != "complete")
+            { 
+                // get location info of HOOPS
+                var g_lat = HOOPS[i]["lat"];
+                var g_lng = HOOPS[i]["lng"];
+                
+                // check distance from shuttle
+                var d = shuttle.distance(g_lat, g_lng);
+
+                
+                // if in range
+                if (d <= 4.0)
+                {
+                    // remove placemark & marker of this HOOP from 3D and 2D map
+                    var features = earth.getFeatures();
+                    HOOPS[i]["marker"].setMap(null);
+                    features.removeChild(HOOPS[i]["placemark"]);
+                    HOOPS[i]["status"] = "complete";   
+                    
+                    // add point
+                    score++;
+                    $('#scoreboard').text(score);
+                }
+            }
+        } 
+    }   
+}  
+
+function change_plane_mode(event)
+{
+    // ensure we have event
+    if (!event)
+    {
+        event = window.event;
+    }
+    
+    // C, c
+    if (event.keyCode == 67 || event.keyCode == 99)
+    {
+        plane_view++;
+        
+        if (plane_view > 2)
+        {
+            plane_view = 0;    
+        } 
+    
+     
+        var url = window.location.href.substring(0, (window.location.href.lastIndexOf("/")) + 1); 
+        
+        if (plane_view == 0)
+        {
+            make_overlay(url);
+        }
+        else if (plane_view == 1)
+        {
+            make_plane(url);
+            earth.getFeatures().removeChild(screenOverlay);
+        }
+        else 
+        {
+            earth.getFeatures().removeChild(placemark);
+        }
+    }
+
+}  
+
+function angle_standard()
+{
+     if (shuttle.headingAngle <= 0) 
+        {
+            shuttle.headingAngle = 360 * Math.PI /180 ;
+        }
+        else if (shuttle.headingAngle * 180 / Math.PI >= 360) 
+        {
+            shuttle.headingAngle = 0;
+        }    
+}
